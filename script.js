5b854cca516e1e2c80484af299e763571bac6db1
Gabriel Dias <158090647+gabrieldiasdevs@users.noreply.github.com>
2024-03-12 19:10:58 -0300

    javascript do cardapio

---
+++ script.js
@@ -0,0 +1,52 @@
+const menu = document.getElementById("menu")
+const cartBtn = document.getElementById("cart-btn")
+const cartModal = document.getElementById("cart-modal")
+const cartItemsContainer = document.getElementById("cart-items")
+const cartTotal = document.getElementById("cart-total")
+const checkoutBtn = document.getElementById("checkout-btn")
+const closeModalBtn = document.getElementById("close-modal-btn")
+const cartCounter = document.getElementById("cart-count")
+const addressInput = document.getElementById("address")
+const addressWarn = document.getElementById("address-warn")
+
+let cart = []
+
+// Abrir o modal do carrinho
+cartBtn.addEventListener("click", function() {
+    cartModal.style.display = "flex"
+})
+
+// Fechar o modal quando clicar fora
+cartModal.addEventListener("click", function(event) {
+    if(event.target === cartModal || event.target === closeModalBtn) {
+        cartModal.style.display = "none"
+    }
+})
+
+// Pegar a classe por perto do clique, Pegar os dados
+menu.addEventListener("click", function(event) {
+    let parentButton = event.target.closest(".add-to-cart-btn")
+
+    if(parentButton) {
+        const name = parentButton.getAttribute("data-name")
+        const price = parseFloat(parentButton.getAttribute("data-price"))
+
+        addToCart(name, price)
+    }
+})
+
+// Função para adicionar no carrinho
+function addToCart(name, price) {
+    const existingItem = cart.find(item => item.name === name)
+
+    if(existingItem) {
+        // Se o item já existe, aumenta a quantidade + 1
+        existingItem.quantity += 1
+    }
+
+    cart.push({
+        name,
+        price,
+        quantity: 1,
+    })
+}
